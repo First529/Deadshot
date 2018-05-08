24a4b5c74277245b78231512a9e427bad4b6efe7
booknap <napasai45@gmail.com>
2018-05-08 13:43:17 +0700

    adjust UI

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -13,21 +13,31 @@ import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import swing.Game;
 
-public class Controller implements Initializable{
+public class Controller implements Initializable {
+
+	@FXML
+	private Button play;
+	@FXML
+	private Button setting;
+	@FXML
+	private Button quit;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+
+	}
 	
 	@FXML
-	Button button;
+	public void handlePlay(ActionEvent event) {
 
-	public void handleConvert(ActionEvent event) {
-	
-		Stage stage = (Stage) button.getScene().getWindow();  
+		Stage stage = (Stage) play.getScene().getWindow();
 		stage.close();
 		try {
 			Game game = new Game();
 			game.setPreferredSize(new Dimension(800, 600));
 			game.setMaximumSize(new Dimension(800, 600));
 			game.setMaximumSize(new Dimension(800, 600));
-		
+
 			JFrame frame = new JFrame("Dead shot");
 			frame.add(game);
 			frame.pack();
@@ -35,20 +45,21 @@ public class Controller implements Initializable{
 			frame.setResizable(false);
 			frame.setLocationRelativeTo(null);
 			frame.setVisible(true);
-		
+
 			game.start();
 		} catch (Exception ex) {
 			System.out.println("Can't open game.");
 		}
-		
-	}
-	
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		
-		
-	}
-	
-	
 
+	}
+
+	@FXML
+	public void handlerSetting() {
+
+	}
+
+	@FXML
+	public void handlerQuit() {
+		System.exit(0);
+	}
 }
