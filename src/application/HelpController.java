03241f42c25f037793a1710f50823cf00e6120ff
dechabhol kotheeranurak <first_dejabhol@hotmail.com>
2018-05-12 12:17:47 +0700

    fixed

---
+++ src/application/HelpController.java
@@ -0,0 +1,32 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class HelpController {
+	
+	@FXML
+	private Button backButton;
+	
+	private Stage stage;
+
+	public void handleBack(ActionEvent event) {
+		stage = (Stage) backButton.getScene().getWindow();
+		stage.close();
+		try {
+			Parent root = (Parent) FXMLLoader.load(getClass().getResource("MenuUI.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.setScene(scene);
+			stage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
